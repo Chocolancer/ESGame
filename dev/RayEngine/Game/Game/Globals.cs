3ecbc2a7a9bf899698e9ea340f04a19822f17cdf
Chocolancer <jasonbolanos92@gmail.com>
2012-12-30 20:02:57 -0800

    Relocated Ray's engine, added new directory

---
+++ dev/RayEngine/Game/Game/Globals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    /// <summary>
+    /// Stores paused bool
+    /// </summary>
+    class Globals
+    {
+        /// <summary>
+        /// Checks if minigame is paused
+        /// </summary>
+        static bool paused = false;
+        /// <summary>
+        /// Checks if minigame is paused
+        /// </summary>
+        public static bool Paused
+        {
+            get
+            {
+                return paused;
+            }
+            set
+            {
+                paused = value;
+            }
+        }
+    }
+}
